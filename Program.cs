ce664c608156d738d1846f3ac96631535edb6bbf
avanysouza <avanysouza@gmail.com>
2024-05-14 18:47:41 +0100

    solucao

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("55481151", "Iphone 15", "IMEI - XR15115", 128);
+
+Nokia nokia = new Nokia("84516151", "NK-5181", "IMEI - SRD1515154", 32);
+
+
+iphone.InstalarAplicativo("Iphone");
+
+nokia.InstalarAplicativo("Facebook");
